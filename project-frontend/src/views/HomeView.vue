85dac58ef06aae81f5cbda607212f91100bda01e
RaulD32 <joserauldiaz779@gmail.com>
2025-03-12 19:40:22 -0500

    limpio de componentes default

--- project-frontend/src/views/HomeView.vue
+++ project-frontend/src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="flex items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-600">
+    <h1 class="text-4xl font-bold text-white drop-shadow-lg animate-bounce">
+      ¡Hola mundo desde el Home! 😱
+    </h1>
   </main>
 </template>
