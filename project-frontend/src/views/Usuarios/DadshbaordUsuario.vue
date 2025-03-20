2c36d6702b329bde40f11f0fc9f6b96a13a3fda7
RaulD32 <joserauldiaz779@gmail.com>
2025-03-20 16:57:13 -0500

    mejoras distribución home vacantes

--- project-frontend/src/views/Usuarios/DadshbaordUsuario.vue
+++ project-frontend/src/views/Usuarios/DadshbaordUsuario.vue
@@ -3,9 +3,11 @@ import Vacantes from '@/components/Vacantes.vue';
 import HeaderComponent from '@/components/HeaderComponent.vue';
 import FooterComponent from '@/components/FooterComponent.vue';
 </script>
+
 <template>
   <HeaderComponent/>
-  <div>    
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6 bg-gray-100">
+    <h2 class="col-span-full text-2xl font-bold text-center mb-6">Vacantes Disponibles en el Sistema</h2>
     <Vacantes />
   </div>
   <FooterComponent/>
