3f94890d0d015dc76c956adae6a15e146b77851b
RaulD32 <joserauldiaz779@gmail.com>
2025-03-20 17:06:11 -0500

    se implementa buscador por nombre y descripción en las vacantes

--- project-frontend/src/views/Usuarios/DadshbaordUsuario.vue
+++ project-frontend/src/views/Usuarios/DadshbaordUsuario.vue
@@ -7,8 +7,13 @@ import FooterComponent from '@/components/FooterComponent.vue';
 <template>
   <HeaderComponent/>
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6 bg-gray-100">
-    <h2 class="col-span-full text-2xl font-bold text-center mb-6">Vacantes Disponibles en el Sistema</h2>
-    <Vacantes />
+    <div class="col-span-full mb-6">
+      <h2 class="text-2xl font-bold text-center mb-4">Vacantes Disponibles en el Sistema</h2>
+      <div class="max-w-md mx-auto">
+       
+      </div>
+    </div>
+    <Vacantes :vacantes="filteredVacantes" />
   </div>
   <FooterComponent/>
 </template>
