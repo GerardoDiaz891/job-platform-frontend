c4028ef2820e736eaccb236a4e666b2bf429d874
Gerardo <VampyrTempestmail.com>
2025-03-20 20:04:36 -0500

    Implementación del interceptor de Token

--- project-frontend/src/services/api.ts
+++ project-frontend/src/services/api.ts
@@ -5,6 +5,15 @@ const apiClient = axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
+// Interceptor para agregar el token a las solicitudes
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token'); // El token debe guardarse en el LocalStorage
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 //GET de todas las vacantes
 export const getVacantes = async () => {
   try {
@@ -16,7 +25,7 @@ export const getVacantes = async () => {
   }
 };
 
-//GET vacante por
+//GET vacante por ID
 export const getVacanteById = async (id) => {
   try {
     const { data } = await apiClient.get(`/api/Vacantes/${id}`);
